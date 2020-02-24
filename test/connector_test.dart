382c05db78871c92dd64e0e131a8061cf02f3c94
Brandon <supernova2468@gmail.com>
2020-02-23 19:33:49 -0600

    tweaking workflow

--- test/connector_test.dart
+++ test/connector_test.dart
@@ -1,10 +1,11 @@
-// import 'package:omsat_app/logic/peers.dart';
-// import 'package:test/test.dart';
-// import 'package:omsat_app/logic/connector.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// void main() {
-//   group('Connector', () {
-//     test(
-//         'connector should try to establish a connection with each peer', () {});
-//   });
-// }
+import 'package:omsat_app/logic/peers.dart';
+import 'package:omsat_app/logic/connector.dart';
+
+void main() {
+  group('Connector', () {
+    test(
+        'connector should try to establish a connection with each peer', () {});
+  }, skip: 'TODO implement this');
+}
